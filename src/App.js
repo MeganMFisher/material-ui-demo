5eb5ea57faa3b8d24569016f7310f67a0ff966d4
meganmfisher <meganmichellefisher@gmail.com>
2017-10-17 15:19:57 -0600

    cleared for demo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import Menu from './components/Menu';
-import DialogBox from './components/DialogBox';
 
 class App extends Component {
   render() {
     return (
       <div >
-        <Menu/>
 
-        <section>
-          <DialogBox/>
-        </section>
       </div>
     );
   }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,66 +1,13 @@
 import React, { Component } from 'react';
-import Drawer from 'material-ui/Drawer';
-import {List, ListItem} from 'material-ui/List';
-
-import IconButton from 'material-ui/IconButton';
-import ActionGrade from 'material-ui/svg-icons/action/grade';
-
-
 
 export default class Menu extends Component {
-constructor(props) {
-    super(props);
-        
-    this.state = {
-        open: false
-    };
-}
     
-handleToggle = () => {
-    this.setState({
-        open: !this.state.open
-    });
-}
-    
-
 render() {
     return (
         <div>
-        <nav>
-          <IconButton onClick={this.handleToggle}>
-            <ActionGrade />
-          </IconButton>
-        </nav>
+          <nav>
 
-      <Drawer
-        docked={false} 
-        width={300} 
-        open={this.state.open} 
-        onRequestChange={(open) => this.setState({open})}>
-        <List>
-          <ListItem primaryText="Home" />
-          <ListItem primaryText="About" />
-          <ListItem
-            primaryText="Store"
-            nestedItems={[
-              <ListItem
-                primaryText="Tops"
-              />,
-              <ListItem
-                primaryText="Bottoms"
-              />,
-              <ListItem
-                primaryText="Accessories"
-                nestedItems={[
-                  <ListItem primaryText="Hats" />,
-                  <ListItem primaryText="Jewelry" />,
-                  <ListItem kprimaryText="Scarves" />,
-                ]}
-              />,
-            ]}
-          />
-        </List>
-      </Drawer>
+          </nav>   
         </div>
     )
 }
